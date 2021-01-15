24aaaaeb685f4974612202779ad7c8dcc87ae85f
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-01-15 13:08:07 -0500

    added sass

--- src/components/NewNoteForm/NewNoteForm.tsx
+++ src/components/NewNoteForm/NewNoteForm.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addNote } from '../../actions/notes';
+import './newNoteForm.scss';
 
 const NewNoteForm = () => {
   const [text, setText] = useState('');
@@ -14,7 +15,7 @@ const NewNoteForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className='new-note-form' onSubmit={handleSubmit}>
       <textarea value={text} onChange={(e) => setText(e.target.value)} />
       <button type='submit'>Add Note</button>
     </form>
