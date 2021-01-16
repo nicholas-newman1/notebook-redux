d17d91663c1f871d4f3c9ccceb8fa556113f416d
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-01-16 14:56:49 -0500

    further styled noteList

--- src/components/NotesList/NoteList.tsx
+++ src/components/NotesList/NoteList.tsx
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import { AppState } from '../../reducers';
 import NoteListItem from '../NoteListItem/NoteListItem';
+import './noteList.scss';
 
 const NoteList: React.FC = () => {
   const notes = useSelector((state: AppState) => state.notes);
 
   return (
-    <ul>
+    <ul className='note-list'>
       {notes.map((note) => (
         <NoteListItem key={note.id} note={note} />
       ))}
