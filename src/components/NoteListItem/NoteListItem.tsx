cf4ffec9d4fdd18d6fa8fc2ace641a0877ee0212
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-01-18 10:05:41 -0500

    added routing to view note

--- src/components/NoteListItem/NoteListItem.tsx
+++ src/components/NoteListItem/NoteListItem.tsx
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { deleteNote } from '../../actions/notes';
 import './noteListItem.scss';
 import TrashCan from '../TrashCan/TrashCan';
+import { useHistory } from 'react-router-dom';
+import { useRef } from 'react';
 
 interface Props {
   note: Note;
@@ -10,14 +12,33 @@ interface Props {
 
 const NoteListItem: React.FC<Props> = ({ note }) => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const deleteBtn = useRef<HTMLButtonElement>(null);
 
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
     const btn = e.currentTarget as HTMLButtonElement;
     dispatch(deleteNote(btn.parentElement!.id));
   };
 
+  const viewNote = (e: React.MouseEvent<HTMLLIElement>, id: string) => {
+    const deleteBtnElements = [
+      deleteBtn.current,
+      deleteBtn.current!.firstElementChild,
+    ];
+
+    const isTargetDeleteBtn =
+      deleteBtnElements.findIndex((el) => (e.target as HTMLElement) === el) !==
+      -1;
+
+    !isTargetDeleteBtn && history.push(`notes/${id}`);
+  };
+
   return (
-    <li className='note-list-item' id={note.id}>
+    <li
+      className='note-list-item'
+      id={note.id}
+      onClick={(e) => viewNote(e, note.id)}
+    >
       <div className='note-list-item__meta'>
         <h2 className='note-list-item__title'>{note.title}</h2>
         <p className='note-list-item__date'>
@@ -25,7 +46,11 @@ const NoteListItem: React.FC<Props> = ({ note }) => {
         </p>
       </div>
 
-      <button className='note-list-item__btn' onClick={handleDelete}>
+      <button
+        ref={deleteBtn}
+        className='note-list-item__btn'
+        onClick={handleDelete}
+      >
         <TrashCan />
       </button>
     </li>
